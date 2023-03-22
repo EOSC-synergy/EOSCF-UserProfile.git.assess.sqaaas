25a820c845934f97e1cda9057c4ed6faa09149c0
orviz <orviz@ifca.unican.es>
2023-03-22 15:41:04 +0100

    Add JePL file structure

--- .sqa/script.pokey-plum-coyote.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/athenarc/EOSCF-UserProfile &&
-    hadolint */Dockerfile* --failure-threshold error
-)
---
+++ .sqa/script.tasty-cream-tortoise.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/athenarc/EOSCF-UserProfile &&
+    hadolint Dockerfile --failure-threshold error
+)
